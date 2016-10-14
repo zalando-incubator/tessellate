f6ea20581bae111de136c757a99920fd57d2bc21
Maximilian Fellner <maximilian.fellner@zalando.de>
2016-10-14 13:04:07 +0200

    fix: element module name parsing

--- tessellate-bundler/src/script-builder/index.js
+++ tessellate-bundler/src/script-builder/index.js
@@ -40,7 +40,7 @@ export function build(element: ElementType): string {
 function toReactScript(element: ElementType | string, props: PropsType = {}, imports: ImportsType = {}): ReactScript {
   // Atomic element, e.g. string content of a headline.
   if (typeof element === 'string')
-    return {script: `"${element}"`, props, imports}
+    return {script: `\`${element}\``, props, imports}
 
   const {className, imported} = parseElementType(element)
   const id = uuid.v4()
@@ -68,30 +68,32 @@ function toReactScripts(elements: ?Array<ElementType | string>, props: PropsType
 }
 
 function parseElementType(element: ElementType): {|className: string; imported?: ImportsType;|} {
-  const parts = element.type.split('.')
+  if (!element.type)
+    throw new ElementProblem('Missing element type on ' + JSON.stringify(element))
 
-  if (parts.length > 2)
-    throw new ElementProblem(`Illegal element type ${element.type}`)
-  // Type has format '<node-module-name>.<component-name>'
-  if (parts.length === 2) {
+  // Try to match '<node-module-name>.<component-name>'
+  const [_, moduleName, componentName] = element.type.match(/^([^\.]+)\.?(.+)?/) || []
+
+  if (moduleName && componentName) {
     // Module imports for this element.
     const imported = {}
     // Add a module import based on the type.
-    const name: string = camelCase(parts[0])
-    const module = parts[0]
-    imported[name] = module
+    const importName: string = camelCase(moduleName)
+    imported[importName] = moduleName
     // If a style attribute is present, add a module import for it.
     const style: ?string = constantCase(element.style)
     if (style) imported[style] = element.style
-    // The React class name is '<imported-module>.<component-name>'
-    const className = `${name}.${parts[1]}`
+    // The React class name is '<import-name>.<component-name>'
+    const className = `${importName}.${componentName}`
     return {className, imported}
   }
   // Type is a literal React component name (e.g. 'div')
-  else {
-    const name: string = camelCase(element.type)
-    const module = element.type
+  else if (moduleName) {
+    const name: string = camelCase(moduleName)
+    const module = moduleName
     const className = `'${name}'`
     return  {className}
   }
+  else
+    throw new ElementProblem(`Illegal element type ${element.type}`)
 }
