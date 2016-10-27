59f7d3a15e1962c28f5a6b99f6b91b65a915566e
Maximilian Fellner <maximilian.fellner@zalando.de>
2016-10-27 14:54:53 +0200

    chore: Update travis deploy configuration.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -9,6 +9,13 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  rm -rf .git
+  git init
+  git clean -dfx
+  git fetch origin
+  git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
+  git checkout $TRAVIS_BRANCH
+
   openssl aes-256-cbc -K $encrypted_987335dd518e_key -iv $encrypted_987335dd518e_iv -in .travis/deploy_key.enc -out deploy_key -d
   chmod 600 deploy_key
   eval `ssh-agent -s`
@@ -20,4 +27,7 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   git config --global user.email "maximilian.fellner@zalando.de"
   git config --global user.name "Maximilian Fellner"
   git config --global push.default simple
+
+  git fetch --tags
+  git branch -u origin/$TRAVIS_BRANCH
 fi
