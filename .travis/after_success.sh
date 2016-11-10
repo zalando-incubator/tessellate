77b4ee0f24b11416a8f44d613db22e2330defb2c
Maximilian Fellner <maximilian.fellner@zalando.de>
2016-11-10 11:01:07 +0100

    chore(travis): Fixed travis deployment hooks yet again.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -13,5 +13,12 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   git config credential.helper "store --file=.git/credentials"
   echo "https://$RELEASE_GH_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git" > .git/credentials
 
+  git config --global user.email "maximilian.fellner@zalando.de"
+  git config --global user.name "Maximilian Fellner"
+  git config --global push.default simple
+
+  npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN} -q
+  npm prune
+
   npm run semantic-release
 fi
--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -10,22 +10,12 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  git config credential.helper "store --file=.git/credentials"
-  echo "https://$RELEASE_GH_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git" > .git/credentials
-
   rm -rf .git
   git init
   git clean -dfx
-  git remote add origin git@github.com:${TRAVIS_REPO_SLUG}.git
+  git remote add origin https://github.com/${TRAVIS_REPO_SLUG}.git
   git fetch origin
   git checkout ${TRAVIS_BRANCH}
-
-  npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN} -q
-  npm prune
-
-  git config --global user.email "maximilian.fellner@zalando.de"
-  git config --global user.name "Maximilian Fellner"
-  git config --global push.default simple
 
   git fetch --tags
   git branch -u origin/${TRAVIS_BRANCH}
