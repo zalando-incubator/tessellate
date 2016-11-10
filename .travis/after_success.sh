556cb5afc067b1ffe370dc1a6cec9806571414a6
Maximilian Fellner <maximilian.fellner@zalando.de>
2016-11-10 10:29:00 +0100

    chore(travis): Fixed travis deployment hooks some more.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # https://github.com/atlassian/lerna-semantic-release/blob/caribou/.travis/after_success.sh
+# http://stackoverflow.com/questions/18027115/committing-via-travis-ci-failing
 
 set -e
 
@@ -9,5 +10,8 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://$RELEASE_GH_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git" > .git/credentials
+
   npm run semantic-release
 fi
--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # https://github.com/atlassian/lerna-semantic-release/blob/caribou/.travis/before_install.sh
+# http://stackoverflow.com/questions/18027115/committing-via-travis-ci-failing
 
 set -e
 
@@ -9,8 +10,8 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  git config credential.helper store
-  echo "https://mfellner:$RELEASE_GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" > ~/.git-credentials
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://$RELEASE_GH_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git" > .git/credentials
 
   rm -rf .git
   git init
