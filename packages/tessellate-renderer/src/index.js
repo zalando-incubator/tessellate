2c5cb4997d820cfaab33d9842c201143e7ae067a
Maximilian Fellner <maximilian.fellner@zalando.de>
2016-11-24 15:16:47 +0100

    refactor(renderer): Remove debug logging.
    affects: tessellate-renderer

--- packages/tessellate-renderer/src/index.js
+++ packages/tessellate-renderer/src/index.js
@@ -4,16 +4,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 import SandboxVM from './SandboxVM'
-import logger from './logger'
 
-const log = logger('render-service')
 // Hack: global.__REQUIRE_FN__ must be defined in webpack.config.
 const requireFn = global.__REQUIRE_FN__ || require
 const sandbox = new SandboxVM({require: requireFn})
 
 export function renderToStaticMarkup(source: string, props: Object = {}): string {
-  log.debug('Execute source code....')
   const {Fragment} = sandbox.run(source)
-  log.debug('Render string with props %o', props)
   return ReactDOMServer.renderToString(<Fragment {...props}/>)
 }
