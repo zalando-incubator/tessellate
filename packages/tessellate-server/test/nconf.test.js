9a040fc6c806e1988af048d9723e3440136add7b
Maximilian Fellner <maximilian.fellner@zalando.de>
2017-01-25 10:27:37 +0100

    test(server): Add nconf tests.
    affects: tessellate-server

---
+++ packages/tessellate-server/test/nconf.test.js
@@ -0,0 +1,26 @@
+// @flow
+
+import nconf from '../src/nconf'
+
+describe('nconf', () => {
+  it('should store a value', () => {
+    const foo = nconf.get('FOO')
+    expect(foo).toBe('bar')
+  })
+
+  it('should store a string value', () => {
+    const foo = nconf.getString('FOO')
+    expect(foo).toBe('bar')
+  })
+
+  it('should store an object value', () => {
+    const bar = nconf.getObject('BAR')
+    expect(bar).toEqual({what: 'is', code: 42})
+  })
+
+  it('should set a value', () => {
+    nconf.set('NEW_VALUE', 41)
+    const value = nconf.get('NEW_VALUE')
+    expect(value).toEqual(41)
+  })
+})
